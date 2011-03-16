d0389159a4a10c7601724c974f9130c1a6338956
alron <alron@bloodmagic.com>
2011-03-15 17:18:21 -0700

    Removed commented wxidc code from old class location.

--- src/main/java/com/wormhole_xtreme/WormholeXTreme.java
+++ src/main/java/com/wormhole_xtreme/WormholeXTreme.java
@@ -235,10 +235,6 @@ public class WormholeXTreme extends JavaPlugin
 		{
 		    return WormholeXTremeCommand.commandCompleteGate(sender, args);
 		}
-//		else if ( commandName.equals("wxidc"))
-//		{
-//			return WormholeXTremeCommand.commandIDC(sender, args);
-//		}
 		return false;
 	}
 

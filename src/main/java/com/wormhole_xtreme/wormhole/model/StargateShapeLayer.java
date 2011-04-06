cfea7b0ab34e9ff8376b4839d7120cf70ea1047d
Lologarithm <JokerSP3@gmail.com>
2011-04-06 00:18:25 -0600

    Finished parsing of new gate shape

--- src/main/java/com/wormhole_xtreme/wormhole/model/StargateShapeLayer.java
+++ src/main/java/com/wormhole_xtreme/wormhole/model/StargateShapeLayer.java
@@ -11,7 +11,7 @@ import com.wormhole_xtreme.wormhole.WormholeXTreme;
 public class StargateShapeLayer 
 {
 	/** The stargate_positions. */
-	public int[][] stargatePositions = null; /*{ {0,2,0}, {0,3,0}, {0,4,0}, 
+	public ArrayList<Integer[]> blockPositions = new ArrayList<Integer[]>(); /*{ {0,2,0}, {0,3,0}, {0,4,0}, 
 		{0,1,1}, {0,5,1}, 
 		{0,0,2}, {0,6,2}, 
 		{0,6,3}, {0,0,3}, 
@@ -24,12 +24,22 @@ public class StargateShapeLayer
 	
 	/** The enter_position. */
 	public int[] enterPosition = null;//{0,0,3};
+
+	/** The enter_position. */
+	public int[] activationPosition = null;//{0,0,3};
+	/** The enter_position. */
+	public int[] irisActivationPosition = null;//{0,0,3};
+	/** The enter_position. */
+	public int[] dialerPosition = null;//{0,0,3};
 	
 	/** The light_positions. */
-	public int[][] lightPositions = null;//{3,4,11,12};
+	public ArrayList<ArrayList<Integer[]>> lightPositions = new ArrayList<ArrayList<Integer[]>>();//{3,4,11,12};
+
+	/** The positions of woosh. First array is the order to activate them. Inner array is list of points */
+	public ArrayList<ArrayList<Integer[]>> wooshPositions = new ArrayList<ArrayList<Integer[]>>();//{3,4,11,12};
 	
 	/** The water_positions. */
-	public int[][] waterPositions = null; /*{ {0,2,1}, {0,3,1}, {0,4,1}, 
+	public ArrayList<Integer[]> portalPositions = new ArrayList<Integer[]>(); /*{ {0,2,1}, {0,3,1}, {0,4,1}, 
 			{0,1,2}, {0,2,2}, {0,3,2}, {0,4,2}, {0,5,2}, 
 			{0,1,3}, {0,2,3}, {0,3,3}, {0,4,3}, {0,5,3}, 
 			{0,1,4}, {0,2,4}, {0,3,4}, {0,4,4}, {0,5,4}, 
@@ -37,10 +47,6 @@ public class StargateShapeLayer
 
 	public StargateShapeLayer(String[] layerLines, int height, int width)
 	{
-		ArrayList<Integer[]> blockPositions = new ArrayList<Integer[]>();
-		ArrayList<Integer[]> portalPositions = new ArrayList<Integer[]>();
-		ArrayList<ArrayList<Integer>> lightPositions = new ArrayList<ArrayList<Integer>>();
-		
 		int numBlocks = 0;
 		
 		// 1. scan all lines for lines beginning with [  - that is the height of the gate
@@ -66,19 +72,31 @@ public class StargateShapeLayer
 					{
 						portalPositions.add(point);
 					}
-					else if ( mod.equals("N") || mod.equals("E") )
+					else if ( mod.equals("N") || mod.equals("E") || mod.equals("A") || mod.equals("D") || mod.equals("IA") )
 					{
 						int[] pointI = new int[3];
 						for (int k = 0; k < 3; k++ )
 							pointI[k] = point[k];
 						
-						if ( block.contains("N") )
+						if ( mod.equals("N") )
 						{
 							signPosition = pointI;
 						}
-						if ( block.contains("E") )
+						if ( mod.equals("E") )
 						{
 							enterPosition = pointI;
+						}
+						if ( mod.equals("A") )
+						{
+							activationPosition = pointI;
+						}
+						if ( mod.equals("D") )
+						{
+							dialerPosition = pointI;
+						}
+						if ( mod.equals("IA") )
+						{
+							irisActivationPosition = pointI;
 						}
 					}
 					else if ( mod.contains("L") )
@@ -87,12 +105,25 @@ public class StargateShapeLayer
 						int light_iteration = Integer.parseInt(light_parts[1]);
 						if ( lightPositions.get(light_iteration) == null )
 						{
-							ArrayList<Integer> new_it = new ArrayList<Integer>();
+							ArrayList<Integer[]> new_it = new ArrayList<Integer[]>();
 							lightPositions.set(light_iteration, new_it);
 						}
 						
-						lightPositions.get(light_iteration).add(numBlocks - 1);
+						lightPositions.get(light_iteration).add(point);
 					}
+					else if ( mod.contains("W") )
+					{
+						String[] w_parts = mod.split("#");
+						int w_iteration = Integer.parseInt(w_parts[1]);
+						if ( wooshPositions.get(w_iteration) == null )
+						{
+							ArrayList<Integer[]> new_it = new ArrayList<Integer[]>();
+							wooshPositions.set(w_iteration, new_it);
+						}
+						
+						wooshPositions.get(w_iteration).add(point);
+					}
+					// A, D, IA, RA, RD
 				}
 				j++;
 			}
@@ -101,35 +132,9 @@ public class StargateShapeLayer
 		//TODO: debug printout for the redstone_activated
 		WormholeXTreme.getThisPlugin().prettyLog(Level.CONFIG, false, "Stargate Sign Position: \"" + Arrays.toString(signPosition) + "\"");
 		WormholeXTreme.getThisPlugin().prettyLog(Level.CONFIG, false, "Stargate Enter Position: \"" + Arrays.toString(enterPosition) + "\"");
-		this.waterPositions = new int[portalPositions.size()][3];
-		for ( int i = 0; i < portalPositions.size(); i++)
-		{
-			int[] point = new int[3];
-			for (int j = 0; j < 3; j++ )
-				point[j] = portalPositions.get(i)[j];
-			this.waterPositions[i] = point;
-		}
-		WormholeXTreme.getThisPlugin().prettyLog(Level.CONFIG, false, "Stargate Portal Positions: \"" + Arrays.deepToString((int[][])this.waterPositions) + "\"");
+		//WormholeXTreme.getThisPlugin().prettyLog(Level.CONFIG, false, "Stargate Portal Positions: \"" + Arrays.deepToString((int[][])this.waterPositions) + "\"");
 		
-		this.lightPositions = new int[lightPositions.size()][];
-		for ( int i = 0; i < lightPositions.size(); i++)
-		{
-			this.lightPositions[i] = new int[lightPositions.get(i).size()];
-			for ( int j = 0; j < this.lightPositions[i].length; j++ )
-			{
-				this.lightPositions[i][j] = lightPositions.get(i).get(j);
-			}
-		}
 		WormholeXTreme.getThisPlugin().prettyLog(Level.CONFIG, false, "Light Material Positions: \"" + lightPositions + "\"");
-			
-		this.stargatePositions = new int[blockPositions.size()][3];
-		for ( int i = 0; i < blockPositions.size(); i++)
-		{
-			int[] point = new int[3];
-			for (int j = 0; j < 3; j++ )
-				point[j] = blockPositions.get(i)[j];
-			this.stargatePositions[i] = point;
-		}
-		WormholeXTreme.getThisPlugin().prettyLog(Level.CONFIG, false, "Stargate Material Positions: \"" + Arrays.deepToString((int[][])this.stargatePositions) + "\"");
+		//WormholeXTreme.getThisPlugin().prettyLog(Level.CONFIG, false, "Stargate Material Positions: \"" + Arrays.deepToString((int[][])this.stargatePositions) + "\"");
 	}
 }
